01a4a2c3bbd67faf9ad16edcce6beafc6e726ae9
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-15 13:49:17 -0500

    created schema folder and file and imported in index.js server file and setup graphiql for dev testing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 const port = process.env.PORT || 5000
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.eventNames.NODE_ENV === 'development'
+}));
 
 app.listen(port, console.log(`Server running on port: ${port}`));
 
