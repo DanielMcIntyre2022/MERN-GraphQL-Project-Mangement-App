e02f3720d13f92909a66ee2d452fb4cdb6d6757d
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-15 17:22:47 -0500

    created mutations graphql function which includes an addClient mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,12 @@
-// const { projects, client, clients } = require('../sampleData');
 
 const Project = require('../models/Project');
 const Client = require('../models/Client');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, 
-    GraphQLSchema, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLID, 
+    GraphQLString, 
+    GraphQLSchema, GraphQLList, 
+    GraphQLNonNull
+} = require('graphql');
 
 // Client Type //
 
@@ -70,6 +72,31 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+// Mutations //
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                });
+                return client.save();
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: mutation
 });
