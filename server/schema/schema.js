41af37e954fb5a93fd2dbe825893f1e433058f24
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-15 14:24:07 -0500

    created graphql client type and RootQuery within schema file

---
+++ server/schema/schema.js
@@ -0,0 +1,33 @@
+const { projects, client, clients } = require('../sampleData');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');
+
+// Client Type //
+
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString }
+    })
+});
+
+// Queries //
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID }},
+            resolve(parentValue, args) {
+                return clients.find(client => client.id === args.id)
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
