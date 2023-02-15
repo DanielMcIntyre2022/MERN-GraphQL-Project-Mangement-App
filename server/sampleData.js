27bb7fbe666b719c1ccf7479bbeb044d009de740
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-15 14:07:19 -0500

    created sample data file for graphql testing

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Social Media App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Tony Stark',
+      email: 'test1@gmail.com',
+      phone: 'fake number',
+    },
+    {
+      id: '2',
+      name: 'Daniel McIntyre',
+      email: 'test2@gmail.com',
+      phone: 'fake number',
+    },
+    {
+      id: '3',
+      name: 'Thor Odinson',
+      email: 'test3@gmail.com',
+      phone: '324-331-4333',
+    },
+    {
+      id: '4',
+      name: 'Steve Rogers',
+      email: 'test4@gmail.com',
+      phone: 'fake number',
+    },
+    {
+      id: '5',
+      name: 'Bruce Banner',
+      email: 'test5@gmail.com',
+      phone: 'fake number',
+    },
+  ];
+  
+  module.exports = { projects, clients };
